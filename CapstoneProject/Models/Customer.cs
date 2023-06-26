e8bfcdea5a2e1409b63579944681889cdf25a102
Thomas-Ng-dev <tng.github@gmail.com>
2023-06-26 16:35:15 -0400

    Add project files.

---
+++ CapstoneProject/Models/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CapstoneProject.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public string City { get; set; }
+        [Required]
+        public string Region { get; set; } // State or Province
+        [Required]
+        public string PostalCode { get; set; }
+        [Required]
+        public string Country { get; set; }
+
+    }
+}
