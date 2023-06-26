002274affea8fdb1784b7117b2911c3666ebfe11
Thomas-Ng-dev <tng.github@gmail.com>
2023-06-26 17:01:26 -0400

    Added migration and created Customers table in sql server.

--- CapstoneProject/Data/ApplicationDbContext.cs
+++ CapstoneProject/Data/ApplicationDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace CapstoneProject.Data
+﻿using CapstoneProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CapstoneProject.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        // Create table in sql server, need to add-migration "COMMENT" in npm. Then update-database.
+        public DbSet<Customer> Customers { get; set; }
     }
 }
