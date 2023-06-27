d442dec8f0d3c05613df79b1ff37355b59fc6b92
Thomas-Ng-dev <tng.github@gmail.com>
2023-06-27 19:55:35 -0400

    Added post method for creating a new Customer

--- CapstoneProject/Controllers/CustomerController.cs
+++ CapstoneProject/Controllers/CustomerController.cs
@@ -7,21 +7,31 @@ namespace CapstoneProject.Controllers
     public class CustomerController : Controller
     {
         // Access ApplicationDbContext which is the sql database
-        private readonly ApplicationDbContext _db;
-        public CustomerController(ApplicationDbContext db)
+        private readonly ApplicationDbContext _database;
+        public CustomerController(ApplicationDbContext database)
         {
-            _db = db;
+            _database = database;
         }
         public IActionResult Index()
         {
             // Store list of all customers from table
-            List<Customer> customerList = _db.Customers.ToList();
+            List<Customer> customerList = _database.Customers.ToList();
             return View(customerList);
         }
-        // Action to go to new path/page, goes to Customer/Create
+        // Action to go to new path/page, goes to Customer/Create URL path
         public IActionResult Create() 
         { 
             return View();
+        }
+        [HttpPost] // The page "Create" uses a post method to pass information
+        public IActionResult Create(Customer newCustomer)
+        {
+            // This executes the insert SQL statement
+            _database.Customers.Add(newCustomer);
+            // Changes to database only applied after SaveChanges
+            _database.SaveChanges();
+            // Redirect back to Customer/Index, can also be used to redirect to another controller
+            return RedirectToAction("Index");
         }
     }
 }
