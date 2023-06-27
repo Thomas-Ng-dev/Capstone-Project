43ccd5541a5975083e173a70134663aeaedbbdbe
Thomas-Ng-dev <tng.github@gmail.com>
2023-06-27 19:45:14 -0400

    Implemented tag helper for Customer

--- CapstoneProject/Models/Customer.cs
+++ CapstoneProject/Models/Customer.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace CapstoneProject.Models
 {
@@ -7,20 +8,28 @@ namespace CapstoneProject.Models
         [Key]
         public int Id { get; set; }
         [Required]
+        [DisplayName("Company Name")] // Tag helper, will display this name when this properpty is used elsewhere
         public string Name { get; set; }
         [Required]
+        [DisplayName("Street Address")]
         public string Address { get; set; }
         [Required]
+        [DisplayName("City")]
         public string City { get; set; }
         [Required]
+        [DisplayName("State/Province")]
         public string Region { get; set; } // State or Province
         [Required]
+        [DisplayName("Postal Code")]
         public string PostalCode { get; set; }
         [Required]
+        [DisplayName("Country")]
         public string Country { get; set; }
         [Required]
+        [DisplayName("Phone Number")]
         public string Phone { get; set; }
         [Required]
+        [DisplayName("E-mail")]
         public string Email { get; set; }
     }
 }
