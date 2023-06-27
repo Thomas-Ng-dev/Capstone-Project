58eca2873755716e6bed8d84417e3c36463944a3
Thomas-Ng-dev <tng.github@gmail.com>
2023-06-26 21:37:13 -0400

    Created a customer view so see all customers.

--- CapstoneProject/Controllers/CustomerController.cs
+++ CapstoneProject/Controllers/CustomerController.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CapstoneProject.Data;
+using CapstoneProject.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CapstoneProject.Controllers
 {
     public class CustomerController : Controller
     {
+        // Access ApplicationDbContext which is the sql database
+        private readonly ApplicationDbContext _db;
+        public CustomerController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            // Store list of all customers from table
+            List<Customer> customerList = _db.Customers.ToList();
+            return View(customerList);
         }
     }
 }
