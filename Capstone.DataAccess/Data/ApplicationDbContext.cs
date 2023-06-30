65ebe22e583656520608a79712e894262e1a5d64
Thomas-Ng-dev <tng.github@gmail.com>
2023-06-29 23:55:38 -0400

    Copied Over main assets to new libraries before large refactor.

---
+++ Capstone.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using CapstoneProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CapstoneProject.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        // Create table in sql server, need to add-migration "COMMENT" in npm. Then update-database.
+        public DbSet<Customer> Customers { get; set; }
+        // Seeding data, add-migration after seeding
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().HasData(
+                new Customer { Id = 1, Name = "Company 1", Address = "123 Street", City = "SomeCity", Region = "Washington Stats", PostalCode = "12345", Country = "United States", Phone = "111-222-3333", Email = "mymail@mail.com" },
+                new Customer { Id = 2, Name = "Company 2", Address = "1234 Street", City = "Argon", Region = "New York", PostalCode = "45678", Country = "United States", Phone = "111-225-3243", Email = "guy@mail.com" },
+                new Customer { Id = 3, Name = "Company 3", Address = "12345 Street", City = "Borat", Region = "Quebec", PostalCode = "J4W2R3", Country = "Canada", Phone = "999-222-3333", Email = "dude@mail.com" }
+                );
+        }
+    }
+}
---
+++ Capstone.Models/Models/Customer.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace CapstoneProject.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Company Name")] // Tag helper, will display this name when this property is used elsewhere, server side
+        public string Name { get; set; }
+        [Required]
+        [DisplayName("Street Address")]
+        public string Address { get; set; }
+        [Required]
+        [DisplayName("City")]
+        public string City { get; set; }
+        [Required]
+        [DisplayName("State/Province")]
+        public string Region { get; set; } // State or Province
+        [Required]
+        [DisplayName("Postal Code")]
+        public string PostalCode { get; set; }
+        [Required]
+        [DisplayName("Country")]
+        public string Country { get; set; }
+        [Required]
+        [DisplayName("Phone Number")]
+        public string Phone { get; set; }
+        [Required]
+        [DisplayName("E-mail")]
+        public string Email { get; set; }
+    }
+}
