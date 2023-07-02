f3feb8c68caf7266fff483b8385ee2f7b2519c58
Thomas-Ng-dev <tng.github@gmail.com>
2023-07-01 23:57:12 -0400

    Product controller created.

---
+++ CapstoneProject/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,88 @@
+﻿using Capstone.DataAccess.Data;
+using Capstone.DataAccess.Repository.IRepository;
+using Capstone.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CapstoneProject.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            List<Product> productList = _unitOfWork.Product.GetAll().ToList();
+            return View(productList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost] 
+        public IActionResult Create(Product newProduct)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(newProduct);
+                _unitOfWork.Save();
+                TempData["success"] = "Product created.";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            // Returns object or null
+            Product? product = _unitOfWork.Product.Get(x => x.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(product);
+                _unitOfWork.Save();
+                TempData["success"] = "Product updated.";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            // Returns object or null
+            Product? product = _unitOfWork.Product.Get(x => x.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+        [HttpPost]
+        public IActionResult Delete(Product product)
+        {
+            _unitOfWork.Product.Remove(product);
+            _unitOfWork.Save();
+            TempData["success"] = "Product deleted.";
+            return RedirectToAction("Index");
+        }
+    }
+}
