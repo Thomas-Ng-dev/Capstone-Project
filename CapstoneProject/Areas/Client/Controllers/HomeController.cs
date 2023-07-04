333cc515e649df6d9d1c2c7defae083df3ff77cb
Thomas-Ng-dev <tng.github@gmail.com>
2023-07-03 20:29:29 -0400

    Created initial details page for products.

--- CapstoneProject/Areas/Client/Controllers/HomeController.cs
+++ CapstoneProject/Areas/Client/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
+using Capstone.DataAccess.Repository.IRepository;
 using Capstone.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace CapstoneProject.Areas.Client.Controllers
@@ -8,15 +10,24 @@ namespace CapstoneProject.Areas.Client.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Customer").ToList();
+            return View(productList);
+        }
+        public IActionResult BulkPricing(int productId)
+        {
+            Product product = _unitOfWork.Product.Get(prod => prod.Id == productId, includeProperties: "Customer");
+            return View(product);
         }
 
         public IActionResult Privacy()
