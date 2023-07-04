de552532b8000aaa3f967367523bc9f57042fcdb
Thomas-Ng-dev <tng.github@gmail.com>
2023-07-04 15:13:59 -0400

    Added new fields to the registration page.

--- CapstoneProject/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ CapstoneProject/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -110,6 +110,13 @@ namespace CapstoneProject.Areas.Identity.Pages.Account
             public string? Role { get; set; }
             [ValidateNever]
             public IEnumerable<SelectListItem> RoleList { get; set; }
+            [Required]
+            public string Name { get; set; }
+            public string? StreetAddress { get; set; }
+            public string? City { get; set; }
+            public string? Province { get; set; }
+            public string? PostalCode { get; set; }
+            public string? PhoneNumber { get; set; }
         }
 
 
@@ -145,6 +152,13 @@ namespace CapstoneProject.Areas.Identity.Pages.Account
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
+                // Add new properties for account
+                user.City = Input.City;
+                user.Name = Input.Name;
+                user.Email = Input.Email;
+                user.PostalCode = Input.PostalCode;
+                user.StreetAddress = Input.StreetAddress;
+                user.Province = Input.Province;
 
                 if (result.Succeeded)
                 {
