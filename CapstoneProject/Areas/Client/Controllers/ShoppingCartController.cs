b445288db6f542362c0aa96bc0318fd187773d4f
Thomas-Ng-dev <tng.github@gmail.com>
2023-07-04 20:45:15 -0400

    Shopping cart creation

---
+++ CapstoneProject/Areas/Client/Controllers/ShoppingCartController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CapstoneProject.Areas.Client.Controllers
+{
+    [Area("Client")]
+    public class ShoppingCartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
